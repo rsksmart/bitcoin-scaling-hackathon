5f4fadb32008ea1d20587eb98b88f2e231e2a7ef
mwhc00 <melvinwonghc@gmail.com>
2023-07-16 12:25:15 +0900

    Smart contracts

---
+++ contracts/erc721/ERC721ContractOwnerBurn.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts@4.9.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.9.0/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.9.0/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts@4.9.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.9.0/utils/Counters.sol";
+
+contract ERC721ContractOwnerBurn is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor(string memory myName, string memory mySymbol)
+        public
+        ERC721(myName, mySymbol)
+    {}
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // Only allow contract owner to burn tokens
+    function burn(uint256 tokenId) public onlyOwner override virtual {
+        require(owner() == msg.sender, "Ownership Assertion: Caller of the function is not the contract owner.");
+        //require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not token owner or approved");
+        _burn(tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ contracts/erc721/ERC721NFT.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts@4.9.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.9.0/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.9.0/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts@4.9.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.9.0/utils/Counters.sol";
+
+contract ERC721NFT is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor(string memory myName, string memory mySymbol)
+        public
+        ERC721(myName, mySymbol)
+    {}
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+	
+	// Mint bulk NFTs for multiple addresses with one token URIs
+    function safeMintOneToMany(address[] memory tos, string memory uri) public onlyOwner {
+        uint i = 0;
+        for(i; i < tos.length; i++) {
+            uint256 tokenId = _tokenIdCounter.current();
+            _tokenIdCounter.increment();
+            _safeMint(tos[i], tokenId);
+            _setTokenURI(tokenId, uri);
+        }
+    }
+
+    // Mint bulk NFTs for multiple addresses with multiple token URIs
+    function safeMintManyToMany(address[] memory tos, string[] memory uris) public onlyOwner {
+        uint i = 0;
+        for(i; i < tos.length; i++) {
+            uint256 tokenId = _tokenIdCounter.current();
+            _tokenIdCounter.increment();
+            _safeMint(tos[i], tokenId);
+            _setTokenURI(tokenId, uris[i]);
+        }
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
